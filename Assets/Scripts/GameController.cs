99fba1e5bdf981ca8779001924b12b71375c7508
Ruoyu Sun <ruoyu@unity3d.com>
2019-01-26 17:44:53 +0200

    Add Colletables

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public int FoodCount { get; set; } = 0;
+    public static GameController instance = null;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(this);
+    }
+
+    void Update()
+    {
+
+    }
+}
