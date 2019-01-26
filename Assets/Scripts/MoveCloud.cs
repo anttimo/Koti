56e9a50489cfc5ae0779e1c3a2ff918295743bc0
Antti Moilanen <anttim@unity3d.com>
2019-01-26 21:35:39 +0200

    Added some clouds to the home scenes and fixed mushroom in the ParallaxLevel.

---
+++ Assets/Scripts/MoveCloud.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MoveCloud : MonoBehaviour
+{
+    public float speed = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DOMoveX(-10, speed);
+    }
+}
