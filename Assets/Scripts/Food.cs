092df196b0ce5831b553b9f40e2f12fb877a790e
Ruoyu Sun <ruoyu@unity3d.com>
2019-01-26 18:24:19 +0200

    Add Tweens

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -1,15 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Food : MonoBehaviour
 {
+
+    private SpriteRenderer sr;
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             GameController.instance.FoodCount++;
-            Destroy(gameObject);
+            sr.DOFade(0, 0.5f);
+            transform.DOMoveY(transform.position.y + 5, 1).OnComplete(() =>
+            {
+                Destroy(gameObject);
+            });
         }
     }
 }
