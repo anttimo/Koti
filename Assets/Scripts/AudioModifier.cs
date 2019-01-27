f993c08a18f6d48132ae5afb02259936eb202aad
Antti Moilanen <anttim@unity3d.com>
2019-01-27 15:02:36 +0200

    Added audio modifier.

---
+++ Assets/Scripts/AudioModifier.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioModifier : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    private Rigidbody2D rb;
+    private float originalPlayerX;
+    private AudioSource music;
+
+    private float originalPositionY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        music = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody2D>();
+        originalPlayerX = player.transform.position.x;
+        originalPositionY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        music.pitch = Mathf.Clamp(music.pitch - 0.00002f * (player.transform.position.x - originalPlayerX), 0, 1);
+    }
+}
